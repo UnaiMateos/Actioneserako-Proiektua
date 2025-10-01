c57975153755935951d2111769759d0e70255770
UnaiMateos <mateos.unai@uni.eus>
2025-10-01 09:41:23 +0200

    docker-proba

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
 import os
 
-class Kalkalagailua:
-    def batu(self, a, b):
-        return a + b
+def main():
+ print("=" * 50)
+ print("GitHub Actions + Docker martxan da!")
+ print("=" * 50)
+ 
+if __name__ == "__main__":
+ main()
