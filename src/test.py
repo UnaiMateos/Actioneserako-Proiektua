c820d8910188022005af3485f55ef4c70fcc1d41
UnaiMateos <mateos.unai@uni.eus>
2025-10-01 10:23:16 +0200

    doscker-proba9

--- src/test.py
+++ src/test.py
@@ -1,5 +1,5 @@
-from main import Kalkulagailua
+from main import Kalkalagailua
 
 def test_batu_2_zenbaki():
-    kalkulagailua = Kalkulagailua()
+    kalkulagailua = Kalkalagailua()
     assert kalkulagailua.batu(2, 3) == 5
